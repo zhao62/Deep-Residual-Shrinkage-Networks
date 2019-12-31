f32d6ce341f0ba598febabd7a3b62e3264feab77
zhao62 <minghang.zhao@gmail.com>
2019-12-31 21:57:53 +0800

    Create DRSN_Keras.py

---
+++ DRSN_Keras.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 23:24:05 2019
+
+Implemented using TensorFlow 1.0.1 and Keras 2.2.1
+ 
+M. Zhao, S. Zhong, X. Fu, et al., Deep Residual Shrinkage Networks for Fault Diagnosis, 
+IEEE Transactions on Industrial Informatics, 2019, DOI: 10.1109/TII.2019.2943898
+
+@author: super_9527
+"""
+
+from __future__ import print_function
+import keras
+import numpy as np
+from keras.datasets import mnist
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
+from keras.layers.core import Lambda
+K.set_learning_phase(1)
+
+# Input image dimensions
+img_rows, img_cols = 28, 28
+
+# The data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+# Noised data
+x_train = x_train.astype('float32') / 255. + 0.5*np.random.random([x_train.shape[0], img_rows, img_cols, 1])
+x_test = x_test.astype('float32') / 255. + 0.5*np.random.random([x_test.shape[0], img_rows, img_cols, 1])
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+
+def abs_backend(inputs):
+    return K.abs(inputs)
+
+def expand_dim_backend(inputs):
+    return K.expand_dims(K.expand_dims(inputs,1),1)
+
+def sign_backend(inputs):
+    return K.sign(inputs)
+
+def pad_backend(inputs, in_channels, out_channels):
+    pad_dim = (out_channels - in_channels)//2
+    return K.spatial_3d_padding(inputs, padding = ((0,0),(0,0),(pad_dim,pad_dim)))
+
+# Residual Shrinakge Block
+def residual_shrinkage_block(incoming, nb_blocks, out_channels, downsample=False,
+                             downsample_strides=2):
+    
+    residual = incoming
+    in_channels = incoming.get_shape().as_list()[-1]
+    
+    for i in range(nb_blocks):
+        
+        identity = residual
+        
+        if not downsample:
+            downsample_strides = 1
+        
+        residual = BatchNormalization()(residual)
+        residual = Activation('relu')(residual)
+        residual = Conv2D(out_channels, 3, strides=(downsample_strides, downsample_strides), 
+                          padding='same', kernel_initializer='he_normal', 
+                          kernel_regularizer=l2(1e-4))(residual)
+        
+        residual = BatchNormalization()(residual)
+        residual = Activation('relu')(residual)
+        residual = Conv2D(out_channels, 3, padding='same', kernel_initializer='he_normal', 
+                          kernel_regularizer=l2(1e-4))(residual)
+        
+        # Calculate global means
+        residual_abs = Lambda(abs_backend)(residual)
+        abs_mean = GlobalAveragePooling2D()(residual_abs)
+        
+        # Calculate scaling coefficients
+        scales = Dense(out_channels, activation=None, kernel_initializer='he_normal', 
+                       kernel_regularizer=l2(1e-4))(abs_mean)
+        scales = BatchNormalization()(scales)
+        scales = Activation('relu')(scales)
+        scales = Dense(out_channels, activation='sigmoid', kernel_regularizer=l2(1e-4))(scales)
+        scales = Lambda(expand_dim_backend)(scales)
+        
+        # Calculate thresholds
+        thres = keras.layers.multiply([abs_mean, scales])
+        
+        # Soft thresholding
+        sub = keras.layers.subtract([residual_abs, thres])
+        zeros = keras.layers.subtract([sub, sub])
+        n_sub = keras.layers.maximum([sub, zeros])
+        residual = keras.layers.multiply([Lambda(sign_backend)(residual), n_sub])
+        
+        # Downsampling (it is important to use the pooL-size of (1, 1))
+        if downsample_strides > 1:
+            identity = AveragePooling2D(pool_size=(1,1), strides=(2,2))(identity)
+            
+        # Zero_padding to match channels (it is important to use zero padding rather than 1by1 convolution)
+        if in_channels != out_channels:
+            identity = Lambda(pad_backend)(identity, in_channels, out_channels)
+        
+        residual = keras.layers.add([residual, identity])
+    
+    return residual
+
+
+# define and train a model
+inputs = Input(shape=input_shape)
+net = Conv2D(8, 3, padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(1e-4))(inputs)
+net = residual_shrinkage_block(net, 1, 8, downsample=True)
+net = BatchNormalization()(net)
+net = Activation('relu')(net)
+net = GlobalAveragePooling2D()(net)
+outputs = Dense(10, activation='softmax', kernel_initializer='he_normal', kernel_regularizer=l2(1e-4))(net)
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=100, epochs=5, verbose=1, validation_data=(x_test, y_test))
+
+# get results
+K.set_learning_phase(0)
+DRSN_train_score = model.evaluate(x_train, y_train, batch_size=100, verbose=0)
+print('Train loss:', DRSN_train_score[0])
+print('Train accuracy:', DRSN_train_score[1])
+DRSN_test_score = model.evaluate(x_test, y_test, batch_size=100, verbose=0)
+print('Test loss:', DRSN_test_score[0])
+print('Test accuracy:', DRSN_test_score[1])
